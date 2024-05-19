e2b98f766db60b6324ce516c6d96a00357268a98
giotentori <giorgiotentori2@gmail.com>
2024-05-19 11:15:19 +0200

    agg

---
+++ Progetto2.0/TEDx-Load-Aggregate-Model.py
@@ -0,0 +1,141 @@
+###### TEDx-Load-Aggregate-Model
+######
+
+import sys
+import json
+import pyspark
+from pyspark.sql.functions import col, collect_list, array_join, array_contains, array_intersect, array
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+
+
+
+##### FROM FILES
+tedx_dataset_path = "s3://communitedx-2024-data/final_list.csv"
+
+###### READ PARAMETERS
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+##### START JOB CONTEXT AND JOB
+sc = SparkContext()
+
+
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+
+    
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+
+
+
+#### READ INPUT FILES TO CREATE AN INPUT DATASET
+tedx_dataset = spark.read \
+    .option("header","true") \
+    .option("quote", "\"") \
+    .option("escape", "\"") \
+    .csv(tedx_dataset_path)
+    
+tedx_dataset.printSchema()
+
+
+#### FILTER ITEMS WITH NULL POSTING KEY
+count_items = tedx_dataset.count()
+count_items_null = tedx_dataset.filter("id is not null").count()
+
+print(f"Number of items from RAW DATA {count_items}")
+print(f"Number of items from RAW DATA with NOT NULL KEY {count_items_null}")
+
+
+## READ THE DETAILS
+details_dataset_path = "s3://communitedx-2024-data/details.csv"
+details_dataset = spark.read \
+    .option("header","true") \
+    .option("quote", "\"") \
+    .option("escape", "\"") \
+    .csv(details_dataset_path)
+
+details_dataset = details_dataset.select(col("id").alias("id_ref"),
+                                         col("description"),
+                                         col("duration"),
+                                         col("publishedAt"))
+
+# AND JOIN WITH THE MAIN TABLE
+tedx_dataset_main = tedx_dataset.join(details_dataset, tedx_dataset.id == details_dataset.id_ref, "left") \
+    .drop("id_ref")
+
+tedx_dataset_main.printSchema()
+
+## READ THE IMAGES
+images_dataset_path = "s3://communitedx-2024-data/images.csv"
+images_dataset = spark.read \
+    .option("header","true") \
+    .option("quote", "\"") \
+    .option("escape", "\"") \
+    .csv(images_dataset_path)
+
+images_dataset = images_dataset.select(col("id").alias("id_ref"),
+                                         col("url").alias("image_url"))
+
+
+# AND JOIN WITH THE MAIN TABLE
+tedx_dataset_main = tedx_dataset_main.join(images_dataset, tedx_dataset_main.id == images_dataset.id_ref, "left") \
+    .drop("id_ref")
+
+tedx_dataset_main.printSchema()
+
+
+# AND JOIN WITH THE MAIN TABLE
+tedx_dataset_main = tedx_dataset.join(images_dataset, tedx_dataset_main.id == images_dataset.id_ref, "left") \
+    .drop("id_ref")
+
+tedx_dataset_main.printSchema()
+
+## READ TAGS DATASET
+tags_dataset_path = "s3://communitedx-2024-data/tags.csv"
+tags_dataset = spark.read.option("header","true").csv(tags_dataset_path)
+
+
+# CREATE THE AGGREGATE MODEL, ADD TAGS TO TEDX_DATASET
+tags_dataset_agg = tags_dataset.groupBy(col("id").alias("id_ref")).agg(collect_list("tag").alias("tags"))
+tags_dataset_agg.printSchema()
+tedx_dataset_agg = tedx_dataset_main.join(tags_dataset_agg, tedx_dataset.id == tags_dataset_agg.id_ref, "left") \
+    .drop("id_ref") \
+    .select(col("id").alias("_id"), col("*")) \
+    .drop("id") \
+
+tedx_dataset_agg.printSchema()
+
+## READ WATCH_NEXT DATASET
+watch_next_dataset_path = "s3://communitedx-2024-data/related_videos.csv"
+watch_next_dataset = spark.read.option("header","true").csv(watch_next_dataset_path) 
+
+
+# ADD WATCH_NEXT TO TEDX_DATASET
+watch_next_dataset = watch_next_dataset.dropDuplicates()
+watch_next_dataset_agg = watch_next_dataset.groupBy(col("id").alias("id_ref")).agg(collect_list("related_id").alias("id_next"),collect_list("title").alias("title_next"))
+watch_next_dataset_agg.printSchema()
+tedx_dataset_agg = tedx_dataset_agg.join(watch_next_dataset_agg, tedx_dataset_agg._id == watch_next_dataset_agg.id_ref, "left") \
+    .select(col("*")) \
+    .drop("id_ref") \
+
+tedx_dataset_agg.printSchema()
+
+#   FILTER TAG FROM THE DATASET
+tedx_dataset_agg = tedx_dataset_agg.filter(~array_contains(tedx_dataset_agg.tags,"design"))
+
+
+write_mongo_options = {
+    "connectionName": "CommuniTedx",
+    "database": "tedx_2024",
+    "collection": "tedx_data",
+    "ssl": "true",
+    "ssl.domain_match": "false"}
+from awsglue.dynamicframe import DynamicFrame
+tedx_dataset_dynamic_frame = DynamicFrame.fromDF(tedx_dataset_agg, glueContext, "nested")
